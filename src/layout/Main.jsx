00fac4ed8e086b7b495917e5ff4d5981a7a80c85
BakiAbdullah <shatil605@gmail.com>
2023-04-30 17:01:34 +0600

    Added hero image

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Header from '../components/shared/header/Header';
 import { Outlet } from 'react-router-dom';
+import Header from '../pages/Shared/Header/Header';
 
 const Main = () => {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: `url("/bgImg.png")`,
+       
+      }}
+      className="bg-cover bg-center min-h-screen"
+    >
       <Header></Header>
       <Outlet></Outlet>
     </div>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layout/Main';
-import Header from '../components/shared/header/Header';
 import Home from '../pages/Home/Home';
 
 const router = createBrowserRouter([
